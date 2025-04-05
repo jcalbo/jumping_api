ac29fa32745dbea53faa41db7d93d17732061b9d
Jorge Calbo <jcalbo@users.noreply.github.com>
2025-04-06 01:14:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 from fastapi import FastAPI
-
+from datetime import datetime
 app = FastAPI()
 
 @app.get("/hi")
 def say_hi():
-    return {"message": "Hola Jumping Girls!!"}
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    texto = f"Hola Jumping Girls!! son las {timestamp}"
+    return {"message": texto}
