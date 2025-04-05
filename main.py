b49d3490f84be58c67640626bdca8f066b15f8f9
Jorge Calbo <jcalbo@users.noreply.github.com>
2025-04-06 01:18:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 from fastapi import FastAPI
 from datetime import datetime
+from zoneinfo import ZoneInfo
+
 app = FastAPI()
 
 @app.get("/hi")
 def say_hi():
-    now = datetime.now()
+    now = datetime.now(ZoneInfo("Europe/Madrid"))
     timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
     texto = f"Hola Jumping Girls!! son las {timestamp}"
     return {"message": texto}
